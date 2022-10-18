5a69a73879e6329310e2ab330a569f4806d7c142
Emily Harris <emily@Chriss-Air.lan>
2022-10-18 19:36:33 -0400

    commit

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,35 +1,5 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type": "Feature",
-"properties": {
-    "id":"3469",
-    "name": "San Francisco International Airport",
-    "city": "San Francisco",
-    "country": "United States",
-    "faa": "SFO",
-    "icao": "KSFO",
-    "alt": "13",
-    "tz-offset": "-8",
-    "dst": "A",
-    "tz": "America/Los_Angeles"},
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-122.375, 37.61899948120117]}}
-
-//G Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    //We turn each feature into a marker on the map
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup("<h2> Airport Code: " + feature.properties.icao + "</h2><hr> <h3>Airpot Name: " + feature.properties.name + "</h3>");
-    }
-}).addTo(map);
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -40,3 +10,13 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//Accessing the airport GeoJSON URL
+let airportData= "https://raw.githubusercontent.com/harrisem95/Mapping_Earthquakes/main/majorAirports.json?token=GHSAT0AAAAAABXO2G3V2KPZNCU64WH5ZZUOY2PGU4Q";
+
+//Grabbing our GeoJSON URL
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    //Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data).addTo(map);
+});
